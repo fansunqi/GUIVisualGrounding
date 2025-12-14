36188b7637b6a375f978a3489a1ecfb85958cd88
Yuxuan Han <hanyx21@mails.tsinghua.edu.cn>
2025-12-14 13:45:19 +0800

    add scripts/eval_pipeline_103.sh

--- examples/qwen2_5_vl_3b_mind2web_grpo_103.sh
+++ examples/qwen2_5_vl_3b_mind2web_grpo_103.sh
@@ -1,7 +1,7 @@
 set -x
 
 MODEL_PATH=/mnt/Shared_06_disk1/fsq/hf_home/hub/models--Qwen--Qwen2.5-VL-3B-Instruct/snapshots/66285546d2b821cf421d4f5eb2576359d3770cd3  # replace it with your local file path
-SAVE_PATH=/mnt/Shared_06_disk1/fsq/gui-r1_exp/mind2web_gt_sim_0_9_grpo_qwen2_5_vl_3b_105
+SAVE_PATH=/mnt/Shared_06_disk1/fsq/gui-r1_exp/mind2web_gt_sim_0_9_grpo_qwen2_5_vl_3b
 CONFIG_PATH=examples/config_mind2web_4090.yaml
 
 # Create SAVE_PATH directory if it doesn't exist
@@ -30,6 +30,6 @@ python3 -m verl.trainer.main \
     data.max_pixels=1258291 \
     data.max_prompt_length=2048 \
     data.max_response_length=1024 \
-    data.val_batch_size=128 \
+    data.val_batch_size=512 \
     data.img_dir=/mnt/Shared_06_disk1/fsq/data/Mind2Web/images \
-    trainer.load_checkpoint_path=/mnt/Shared_06_disk1/fsq/gui-r1_exp/mind2web_gt_sim_0_9_grpo_qwen2_5_vl_3b_105/global_step_205 # resume
+    trainer.load_checkpoint_path=/mnt/Shared_06_disk1/fsq/gui-r1_exp/mind2web_gt_sim_0_9_grpo_qwen2_5_vl_3b/global_step_325
---
+++ scripts/eval_pipeline_103.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -x
+cd /home/fsq/gui_agent/GUI-R1-Evol-2/scripts/
+
+SAVE_NAME=mind2web_gt_sim_0_9_grpo_qwen2_5_vl_3b
+
+export TORCH_COMPILE_CACHE=/mnt/Shared_06_disk1/fsq/vllm_cache
+
+# 遍历ckpt编号，从1到10为例
+ckpt_numbers=(200 250 300 325 350 375 400 415)
+for ckpt_num in "${ckpt_numbers[@]}"; do
+    echo "Processing ckpt number: $ckpt_num"
+
+    LOCAL_DIR=/mnt/Shared_06_disk1/fsq/gui-r1_exp/$SAVE_NAME/global_step_$ckpt_num/actor
+    LOCAL_HF_DIR=$LOCAL_DIR/huggingface
+    OUTPUT_DIR=/home/fsq/gui_agent/GUI-R1-Evol-2/guir1/outputs
+
+    if ! ls "$LOCAL_HF_DIR"/*.safetensors 1> /dev/null 2>&1; then
+        echo "No .safetensors files found in $LOCAL_HF_DIR, running convert.py..."
+        python model_merger.py --local_dir $LOCAL_DIR
+    fi
+    
+    cd ..
+    cd guir1
+    # rm -rf ~/.cache/torch/inductor
+    # rm -rf .torch_inductor
+    python inference/inference_vllm_mind2web.py \
+        --model_path $LOCAL_HF_DIR \
+        --data_path /mnt/Shared_06_disk1/fsq/data/Mind2Web/metadata/hf_test_full.json \
+        --image_dir /mnt/Shared_06_disk1/fsq/data/Mind2Web/images \
+        --output_name ${SAVE_NAME}_global_step_$ckpt_num \
+        --num_actor 2
+    python eval/eval_mind2web_point.py \
+        --pred_path $OUTPUT_DIR/${SAVE_NAME}_global_step_$ckpt_num/hf_test_full.json
+    cd ..
+    cd scripts  
+done
