214d128e1920a0c3283aeac9bf9635e2095233f7
Yuxuan Han <hanyx21@mails.tsinghua.edu.cn>
2025-12-16 16:28:34 +0800

    add gt scripts on h20

--- examples/qwen2_5_vl_3b_mind2web_grpo_h20.sh
+++ examples/qwen2_5_vl_3b_mind2web_grpo_h20.sh
@@ -1,6 +1,6 @@
 set -x
 
-EXP_NAME=mind2web_ws_sim_0_9_grpo_qwen2_5_vl_3b
+EXP_NAME=mind2web_gt_grpo_qwen2_5_vl_3b
 MODEL_PATH=/root/cache/hub/models--Qwen--Qwen2.5-VL-3B-Instruct/snapshots/66285546d2b821cf421d4f5eb2576359d3770cd3  # replace it with your local file path
 SAVE_PATH=/root/datasets/fsq/gui_r1_exp/${EXP_NAME}
 CONFIG_PATH=examples/config_mind2web_h20.yaml
@@ -21,14 +21,14 @@ mkdir -p "${TENSORBOARD_DIR}"
 
 python3 -m verl.trainer.main \
     config=${CONFIG_PATH} \
-    data.train_files=/root/cache/hub/datasets--fansunqi--mind2web_adapted/snapshots/ad8ddfb1e87e12cb0001ce683cc041d2896795a2/hf_train_ws_sim_0.9.json \
+    data.train_files=/root/cache/hub/datasets--fansunqi--mind2web_adapted/snapshots/d9fc8ffd5d9e5d82ebfaaa51a2ed35b6874add77/Mind2Web/metadata/hf_train.json \
     data.val_files=/root/cache/hub/datasets--fansunqi--mind2web_adapted/snapshots/d9fc8ffd5d9e5d82ebfaaa51a2ed35b6874add77/Mind2Web/metadata/hf_test_task.json \
     data.img_dir=/root/cache/hub/datasets--fansunqi--mind2web_adapted/snapshots/d9fc8ffd5d9e5d82ebfaaa51a2ed35b6874add77/Mind2Web/images \
     data.system_prompt="${SYSTEM_PROMPT}" \
     worker.actor.model.model_path=${MODEL_PATH} \
     worker.rollout.tensor_parallel_size=1 \
     worker.rollout.enable_chunked_prefill=false \
-    worker.reward.compute_score=r1ws \
+    worker.reward.compute_score=r1gui \
     trainer.experiment_name=${EXP_NAME} \
     trainer.n_gpus_per_node=8 \
     trainer.save_checkpoint_path=${SAVE_PATH} \
