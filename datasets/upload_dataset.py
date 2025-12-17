a8c04f6f494d584aca8aaf3041c33428ba790c30
Yuxuan Han <hanyx21@mails.tsinghua.edu.cn>
2025-12-17 09:19:52 +0800

    revise upload_dataset.py

--- datasets/upload_dataset.py
+++ datasets/upload_dataset.py
@@ -3,10 +3,17 @@ import logging
 
 api = HfApi()
 api.upload_file(
-    path_or_fileobj="/mnt/Shared_05_disk/fsq/gui_agent_data/Mind2Web/metadata/hf_train_ws_sim_0.9.json",  # 本地数据路径
-    path_in_repo="metadata/hf_train_ws_sim_0.9.json",
-    repo_id="fansunqi/mind2web_adapted",  # 数据集仓库名
+    path_or_fileobj="/data/fsq/gui_agent_data/Mind2Web/metadata/hf_test_full.json",  # 本地数据路径
+    path_in_repo="metadata/hf_test_full.json",
+    repo_id="fansunqi/Mind2Web_R1",  # 数据集仓库名
     repo_type="dataset",
-    commit_message="Upload dataset directly"
+    commit_message="fix hf_test_full.json"
 )
 
+# api.upload_file(
+#     path_or_fileobj="/data/fsq/gui_agent_data/Mind2Web/images.tar.gz",  # 本地数据路径
+#     path_in_repo="images.tar.gz",
+#     repo_id="fansunqi/Mind2Web_R1",  # 数据集仓库名
+#     repo_type="dataset",
+#     commit_message="Upload dataset directly"
+# )
